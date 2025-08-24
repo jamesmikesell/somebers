553b25154ed70b422aecb6f2838ca8b82bbf5dcd
James Mikesell <james.mikesell@gmail.com>
2025-08-24 15:19:31 -0400

    adding menu, making color mode toggle-able

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppVersion } from './app-version';
 import { CelebrationComponent } from "./component/celebration/celebration";
+import { MATERIAL_IMPORTS } from './material-imports';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CelebrationComponent],
+  imports: [RouterOutlet, CelebrationComponent, ...MATERIAL_IMPORTS],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -13,6 +14,23 @@ export class App {
 
   AppVersion = AppVersion;
 
-  constructor() { }
+  colorModes = [
+    { mode: 'auto', scheme: 'light dark', label: 'Auto', icon: 'brightness_auto' },
+    { mode: 'light', scheme: 'light', label: 'Light', icon: 'light_mode' },
+    { mode: 'dark', scheme: 'dark', label: 'Dark', icon: 'dark_mode' }
+  ];
+  currentModeIndex = 0;
+
+  get currentColorMode() {
+    return this.colorModes[this.currentModeIndex];
+  }
+
+  constructor() {
+  }
+
+  toggleColorMode() {
+    this.currentModeIndex = (this.currentModeIndex + 1) % this.colorModes.length;
+    document.body.style.colorScheme = this.currentColorMode.scheme;
+  }
 }
 
--- src/app/material-imports.ts
+++ src/app/material-imports.ts
@@ -1,8 +1,9 @@
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 export const MATERIAL_IMPORTS = [
   FormsModule,
@@ -10,4 +11,5 @@ export const MATERIAL_IMPORTS = [
   MatInputModule,
   MatIconModule,
   MatButtonModule,
+  MatMenuModule,
 ] as const;
