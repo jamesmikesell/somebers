035999ae44630e719721ffe25374fedb75fe5063
James Mikesell <james.mikesell@gmail.com>
2025-08-27 22:14:46 +0000

    allowing double tap to clear a cell

--- src/app/directive/hammer/hammer-swipe.directive.ts
+++ src/app/directive/hammer/hammer-swipe.directive.ts
@@ -9,19 +9,27 @@ import Hammer from 'hammerjs';
 export class HammerSwipeDirective implements OnInit, OnDestroy {
   @Output() swipeUp = new EventEmitter<HammerInput>();
   @Output() swipeDown = new EventEmitter<HammerInput>();
+  @Output() tap = new EventEmitter<HammerInput>();
+  @Output() doubleTap = new EventEmitter<HammerInput>();
 
   private hammer: HammerManager | undefined;
 
   constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
-    this.hammer = new Hammer(this.el.nativeElement);
+    this.hammer = new Hammer.Manager(this.el.nativeElement);
 
-    this.hammer.get('swipe').set({
+    this.hammer.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
+    this.hammer.add(new Hammer.Tap({ event: 'tap' }));
+    this.hammer.get('doubletap').recognizeWith('tap');
+    this.hammer.get('tap').requireFailure('doubletap');
+    this.hammer.add(new Hammer.Swipe({
+      event: 'swipe',
       direction: Hammer.DIRECTION_VERTICAL,
       threshold: 20,
       velocity: 0.15
-    });
+    }));
+
 
     this.hammer.on('swipeup', (event: HammerInput) => {
       this.swipeUp.emit(event);
@@ -29,6 +37,14 @@ export class HammerSwipeDirective implements OnInit, OnDestroy {
 
     this.hammer.on('swipedown', (event: HammerInput) => {
       this.swipeDown.emit(event);
+    });
+
+    this.hammer.on('tap', (event: HammerInput) => {
+      this.tap.emit(event);
+    });
+
+    this.hammer.on('doubletap', (event: HammerInput) => {
+      this.doubleTap.emit(event);
     });
   }
 
