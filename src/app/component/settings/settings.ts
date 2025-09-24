558c8e11ab67b03aa245e2621d47ac575e8d8ef3
James Mikesell <james.mikesell@gmail.com>
2025-09-24 12:13:11 +0000

    style(settings): putting settings in cards to visually group them

--- src/app/component/settings/settings.ts
+++ src/app/component/settings/settings.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -22,6 +23,7 @@ import { InstallComponent } from '../install/install.component';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatCardModule,
     MatSlideToggleModule,
     MatSelectModule,
     RouterLink,
